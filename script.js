6414751eacc4984c12449be62a624c60fa402d64
Jyoti Deveda <jyoti@gkmit.co>
2024-10-25 11:19:17 +0530

    feat: add function to get distinct roles

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const executeQuery = async () => {
 		console.log("Connected successfully");
 		const db = client.db("pillpal");
 		const collection = db.collection("accounts");
+
+		// get the distinct roles
+		const data = await getDistinctRoles(collection);
+		console.log("Roles: ", data);
 	} catch (err) {
 		console.log(err);
 	}
@@ -19,6 +23,43 @@ const executeQuery = async () => {
 executeQuery()
 	.then(() => console.log("Done with executing queries"))
 	.catch((err) => console.log("Error occurred: ", err));
+
+// get distinct roles
+const getDistinctRoles = async (collection) => {
+	try {
+		const result = await collection.aggregate([
+			{
+				$project: {
+					arr: {
+						$objectToArray: "$roles",
+					},
+				},
+			},
+			{
+				$unwind: "$arr",
+			},
+			{
+				$group: {
+					_id: "$arr.k",
+					count: {
+						$count: {},
+					},
+				},
+			},
+		]);
+
+		const roles = [];
+		for await (const doc of result) {
+			roles.push(doc._id);
+			// console.x`log(doc);
+		}
+		console.log("Roles: ", roles);
+
+		return roles;
+	} catch (err) {
+		console.log("Error in getting roles: ", err);
+	}
+};
 
 module.exports = {
 	executeQuery,
