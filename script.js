2024b44b4b7572c8cf756fefba2274fc656b48aa
Jyoti Deveda <jyoti@gkmit.co>
2024-10-25 11:19:56 +0530

    feat: create a collection of roles

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const executeQuery = async () => {
 		// get the distinct roles
 		const data = await getDistinctRoles(collection);
 		console.log("Roles: ", data);
+
+		//create collection of the roles
+		await createRoles(db, data);
 	} catch (err) {
 		console.log(err);
 	}
@@ -58,6 +61,27 @@ const getDistinctRoles = async (collection) => {
 		return roles;
 	} catch (err) {
 		console.log("Error in getting roles: ", err);
+	}
+};
+
+// create roles collection
+const createRoles = async (db, roles_list) => {
+	console.log("Roles list:", roles_list);
+	const roles_data = [];
+	roles_list.forEach((role) => {
+		roles_data.push({ name: role });
+	});
+
+	// console.log("Roles data: ", roles_data);
+
+	try {
+		await db.createCollection("roles");
+		const insertedRoles = await db
+			.collection("roles")
+			.insertMany(roles_data);
+		// console.log("Inserted roles: ", insertedRoles);
+	} catch (err) {
+		console.log(err);
 	}
 };
 
